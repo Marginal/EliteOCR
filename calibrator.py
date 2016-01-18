Marginal/EliteOCR

commit 3ffb945a9b13fda6601390917afc2607b1191570

Fixes for user-edited commodity list.
Load and save user-edited commodities.json from storage path, not
app-path.
Make commodity table editor a little more friendly.

calibrator.py
@@ -8,11 +8,10 @@ from PyQt4.QtCore import QSettings, QThread, SIGNAL
 from PyQt4.QtGui import QColor
 from settings import Settings
 from imageprocessing import contBright
-from settings import Settings
 from Levenshtein import ratio, distance
 import sys
 import os
-from os.path import isdir
+from os.path import isdir, join
 from os import listdir
 from collections import Counter
 from colorthief import ColorThief
@@ -131,11 +130,12 @@ class Calibrator(QThread):
         levels = {u"eng": [u'LOW', u'MED', u'HIGH'],
                   u"deu": [u'NIEDRIG', u'MITTEL', u'HOCH'], 
                   u"fra": [u'FAIBLE', u'MOYEN', u'ÉLEVÉ']}
-        file = codecs.open(self.settings.app_path + os.sep +"commodities.json", 'r', "utf-8")
-        comm_list = json.loads(file.read())
-        file.close()
-        #print self.comm_list
-        #self.comm_list.sort(key = len)
+        try:
+            with codecs.open(join(self.settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                comm_list = json.loads(h.read())
+        except:
+            with codecs.open(join(self.settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                comm_list = json.loads(h.read())
         if language == "big" or language == "eng":
             comm_list = [k for k, v in comm_list.iteritems()]
         else:

xmloutput.py
@@ -3,11 +3,12 @@ import codecs
 import json
 import sys
 from datetime import datetime
-from os.path import dirname
+from os.path import dirname, join
 
 class XMLOutput():
     def __init__(self, language, input, output, item, result, system, w, h, translate):
         self.lang = language
+        self.settings = Settings()
 
         if getattr(sys, 'frozen', False):
             self.app_path = dirname(sys.executable)
@@ -56,8 +57,12 @@ class XMLOutput():
         
     def translate(self, result):
         language = self.lang
-        file = codecs.open(self.app_path + os.sep +"commodities.json", 'r')
-        self.comm_list = json.loads(file.read())
+        try:
+            with codecs.open(join(self.settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
+        except:
+            with codecs.open(join(self.settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
         levels = {u"LOW":{u"deu":u"NIEDRIG", u"fra":u"FAIBLE"},
                   u"MED":{u"deu":u"MITTEL", u"fra":u"MOYEN"},
                   u"HIGH":{u"deu":u"HOCH", u"fra":u"ÉLEVÉ"}}

editordialog.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
-from PyQt4.QtGui import QDialog, QTableWidgetItem
+from PyQt4.QtGui import QApplication, QDialog, QTableWidgetItem
 from editorUI import Ui_Editor
 import os
+from os.path import join
 import json
 import codecs
+
+try:
+    _encoding = QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QApplication.translate(context, text, disambig)
+
 
 class EditorDialog(QDialog, Ui_Editor):
     def __init__(self, settings):
@@ -14,16 +24,19 @@ class EditorDialog(QDialog, Ui_Editor):
         self.add_button.clicked.connect(self.addCommodity)
         self.delete_button.clicked.connect(self.deleteCommodity)
 
-        file = codecs.open(self.settings.app_path + ""+ os.sep +"commodities.json", 'r', "utf-8")
-        file_content = file.read()
-        commdict = json.loads(file_content)
-        file.close()
+        try:
+            with codecs.open(join(self.settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                commdict = json.loads(h.read())
+        except:
+            with codecs.open(join(self.settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                commdict = json.loads(h.read())
         # WTF? Clean it!
         titles = []
         for k, v in commdict.iteritems():
             titles = commdict[k].keys()
             break
-        titles.remove("rare")
+        if "rare" in titles:
+            titles.remove("rare")
         if unicode(self.settings["ocr_language"]) in titles:
             titles.remove(unicode(self.settings["ocr_language"]))
             titles.insert(0,unicode(self.settings["ocr_language"]))
@@ -33,7 +46,7 @@ class EditorDialog(QDialog, Ui_Editor):
         totable = []
         for k, v in commdict.iteritems():
             rest = [commdict[k][i] for i in titles]
-            totable.append([commdict[k]["rare"], k] + rest)
+            totable.append([_translate("EliteOCR", commdict[k].get("rare", False) and "Yes" or "No", None), k] + rest)
             
         #print totable
         totable.sort(key=lambda x: x[1])
@@ -47,7 +60,8 @@ class EditorDialog(QDialog, Ui_Editor):
         
     def addCommodity(self):
         self.table.setRowCount(self.table.rowCount()+1)
-        
+        self.table.selectRow(self.table.rowCount()-1)
+
     def deleteCommodity(self):
         self.table.removeRow(self.table.currentRow())
     
@@ -59,18 +73,15 @@ class EditorDialog(QDialog, Ui_Editor):
         for row in xrange(all_rows):
             if (not self.table.item(row,0) is None) and (not self.table.item(row,1) is None):
                 save_dict[unicode(self.table.item(row,1).text())] = {}
-                save_dict[unicode(self.table.item(row,1).text())][unicode(self.table.horizontalHeaderItem(0).text())] = unicode(self.table.item(row,0).text())
+                if self.table.item(row,0).text().toLower() == _translate("EliteOCR", "Yes", None).toLower():
+                    save_dict[unicode(self.table.item(row,1).text())][unicode(self.table.horizontalHeaderItem(0).text())] = True
                 for col in xrange(2,all_cols):
                     if not self.table.item(row,col) is None:
                         save_dict[unicode(self.table.item(row,1).text())][unicode(self.table.horizontalHeaderItem(col).text())] = unicode(self.table.item(row,col).text())
                     else:
                         save_dict[unicode(self.table.item(row,1).text())][unicode(self.table.horizontalHeaderItem(col).text())] = u""
-                    #line = self.result_table.item(row,9).text()
-        #print save_dict
-        
-        file = codecs.open(self.settings.app_path + ""+ os.sep +"commodities.json", 'w', "utf-8")
-        file.write(json.dumps(save_dict, indent=2, separators=(',', ': '), ensure_ascii=False))
-        file.close()
-        
+
+        with codecs.open(join(self.settings.storage_path, "commodities.json"), 'w', "utf-8") as h:
+            h.write(json.dumps(save_dict, indent=2, sort_keys=True, separators=(',', ': '), ensure_ascii=False))
+
         self.close()
-        

engine.py
@@ -2,7 +2,7 @@
 from __future__ import division
 import sys
 import os
-from os.path import isfile
+from os.path import isfile, join
 import re
 import cv2
 import json
@@ -473,7 +473,7 @@ class MLP:
         #print self.result
 
 class Levenshtein:
-    def __init__(self, ocr_data, path, language = "big", levels = True):
+    def __init__(self, ocr_data, settings, language = "big", levels = True):
         if language == "big":
             self.lang = u"eng"
         else:
@@ -482,11 +482,12 @@ class Levenshtein:
         self.levels = {u"eng": [u'LOW', u'MED', u'HIGH'],
                        u"deu": [u'NIEDRIG', u'MITTEL', u'HOCH'], 
                        u"fra": [u'FAIBLE', u'MOYEN', u'ÉLEVÉ']}
-        file = codecs.open(path + ""+ os.sep +"commodities.json", 'r', "utf-8")
-        self.comm_list = json.loads(file.read())
-        file.close()
-        #print self.comm_list
-        #self.comm_list.sort(key = len)
+        try:
+            with codecs.open(join(settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
+        except:
+            with codecs.open(join(settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
         if language == "big" or language == "eng":
             self.comm_list = [k for k, v in self.comm_list.iteritems()]
         else:

export.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+from os.path import join
 import codecs
 import json
 from datetime import datetime, timedelta
@@ -17,8 +18,12 @@ class Export:
     
     def translate(self, input):
         language = unicode(self.parent.settings["ocr_language"])
-        file = codecs.open(self.parent.settings.app_path + ""+ os.sep +"commodities.json", 'r')
-        self.comm_list = json.loads(file.read())
+        try:
+            with codecs.open(join(self.parent.settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
+        except:
+            with codecs.open(join(self.parent.settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
         if language == "big" or language == "eng":
             return input
         else:

ocr.py
@@ -39,7 +39,7 @@ class OCR():
         #for line in market.result:
         #    print line.name
         if levenshtein:
-            Levenshtein(market.result, self.settings.app_path, self.lang, levels)
+            Levenshtein(market.result, self.settings, self.lang, levels)
         return market.result
     
     def makeStationImgClean(self, station):

ocrmethods.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from os.path import join
 import cv2
 import math
 import tesseract
@@ -358,7 +359,7 @@ class TesseractMarket1:
         return linelist
 
 class Levenshtein:
-    def __init__(self, ocr_data, path, language = "big"):
+    def __init__(self, ocr_data, settings, language = "big"):
         if language == "big":
             self.lang = u"eng"
         else:
@@ -367,11 +368,12 @@ class Levenshtein:
         self.levels = {u"eng": [u'LOW', u'MED', u'HIGH'],
                        u"deu": [u'NIEDRIG', u'MITTEL', u'HOCH'], 
                        u"fra": [u'FAIBLE', u'MOYEN', u'ÉLEVÉ']}
-        file = codecs.open(path + os.sep + "commodities.json", 'r', "utf-8")
-        self.comm_list = json.loads(file.read())
-        file.close()
-        #print self.comm_list
-        #self.comm_list.sort(key = len)
+        try:
+            with codecs.open(join(settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
+        except:
+            with codecs.open(join(settings.app_path, "commodities.json"), 'r', "utf-8") as h:
+                self.comm_list = json.loads(h.read())
         if language == "big" or language == "eng":
             self.comm_list = [k for k, v in self.comm_list.iteritems()]
         else:

commit cf3d89fd397f527a53d47feb01fe72d5abffe4ab

Further fixes for user-edited commodity list.
Fixes bugs introduced in 3ffb945.

calibrator.py
@@ -131,11 +131,11 @@ class Calibrator(QThread):
                   u"deu": [u'NIEDRIG', u'MITTEL', u'HOCH'], 
                   u"fra": [u'FAIBLE', u'MOYEN', u'ÉLEVÉ']}
         try:
-            with codecs.open(join(self.settings.storage_path, "commodities.json"), 'r', "utf-8") as h:
-                comm_list = json.loads(h.read())
+            with codecs.open(join(self.settings.storage_path, "commodities.json"), 'r', "utf-8") as f:
+                comm_list = json.loads(f.read())
         except:
-            with codecs.open(join(self.settings.app_path, "commodities.json"), 'r', "utf-8") as h:
-                comm_list = json.loads(h.read())
+            with codecs.open(join(self.settings.app_path, "commodities.json"), 'r', "utf-8") as f:
+                comm_list = json.loads(f.read())
         if language == "big" or language == "eng":
             comm_list = [k for k, v in comm_list.iteritems()]
         else:

xmloutput.py
@@ -4,6 +4,7 @@ import json
 import sys
 from datetime import datetime
 from os.path import dirname, join
+from settings import Settings
 
 class XMLOutput():
     def __init__(self, language, input, output, item, result, system, w, h, translate):